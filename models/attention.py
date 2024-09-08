Fix gradient checkpointing

models/attention.py
@@ -17,14 +17,7 @@ class MultiheadSelfAttention(nn.Module):
         self.num_heads = num_heads
         self.head_dim = embedding_dim // self.num_heads
         self.proj_out = nn.Linear(embedding_dim, embedding_dim, bias=proj_out_bias)
-        self.gradient_checkpointing = False
     
-    
-    def custom(self, module):
-        def custom_forward(*inputs):
-            inputs =  module(inputs[0])
-            return inputs
-        return custom_forward
     
     def forward(self, x: torch.Tensor, cond: torch.Tensor=None, lookahead_mask: bool=False) -> torch.Tensor:
         # x: (n, seq_len, embedding_dim)
@@ -34,18 +27,13 @@ class MultiheadSelfAttention(nn.Module):
        
         if cond is None:
             cond = x
-
+            
         if len(cond.shape) < len(x.shape):
             cond = cond.unsqueeze(1)
         
-        if self.gradient_checkpointing:
-            q = checkpoint.checkpoint(self.custom(self.proj_q), x, use_reentrant=False)
-            k = checkpoint.checkpoint(self.custom(self.proj_k), cond, use_reentrant=False)
-            v = checkpoint.checkpoint(self.custom(self.proj_v), cond, use_reentrant=False)
-        else:
-            q = self.proj_q(x)
-            k = self.proj_k(cond)
-            v = self.proj_v(cond)
+        q = self.proj_q(x)
+        k = self.proj_k(cond)
+        v = self.proj_v(cond)
             
         # (batch_size, seq_len, embedding_dim) -> (n, seq_len, num_heads, head_dim) -> (n, num_heads, seq_len, head_dim)
         q = q.view(*q.shape[:2], self.num_heads, self.head_dim).permute(0, 2, 1, 3)
@@ -68,8 +56,5 @@ class MultiheadSelfAttention(nn.Module):
         # (n, num_heads, seq_len, head_dim) -> (n, seq_len, num_heads, head_dim) -> (n, seq_len, embedding_dim)
         attn_weights = attn_weights.transpose(1, 2).reshape((batch_size, seq_len, embedding_dim))
 
-        if self.gradient_checkpointing:
-            out = checkpoint.checkpoint(self.custom(self.proj_out), attn_weights, use_reentrant=False)
-        else:
-            out = self.proj_out(attn_weights)
+        out = self.proj_out(attn_weights)
         return out

models/unet.py
@@ -70,22 +70,29 @@ class UNet_AttentionBlock(nn.Module):
             self.attn2.proj_k.parametrizations.weight[0].enabled = True
             self.attn2.proj_v.parametrizations.weight[0].enabled = True
             self.attn2.proj_out.parametrizations.weight[0].enabled = True
-            
-            
-            
-
+        
+        self.gradient_checkpointing = False
+    
     def forward(self, x: torch.Tensor, cond: torch.Tensor) -> torch.Tensor:
         residual_x = x
         x = self.layernorm_1(x)
-        x = self.attn1(x)
+        if self.gradient_checkpointing:
+            x = checkpoint.checkpoint(self.attn1, x, use_reentrant=False)
+        else:
+            x = self.attn1(x)
         x += residual_x
         
         residual_x = x
-        x = self.attn2(self.layernorm_2(x), cond=cond)
+        x = self.layernorm_2(x)
+        if self.gradient_checkpointing:
+            x = checkpoint.checkpoint(self.attn2, x, cond, use_reentrant=False)
+        else:
+            x = self.attn2(x, cond=cond)
         x += residual_x
         
         residual_x = x
-        x = self.ffn(self.layernorm_3(x))
+        x = self.layernorm_3(x)
+        x = self.ffn(x)
 
         x += residual_x
         
@@ -128,7 +135,6 @@ class UNet_ResBlock(nn.Module):
         h = self.groupnorm_2(h)
         h = self.silu_2(h)
         h = self.conv_2(h)
-
 
         x = self.proj_input(x)
         
@@ -307,7 +313,15 @@ class UNet(nn.Module):
         
     def gradient_checkpointing_enabled(self):
         for name, module in self.encoder.named_modules():
-            if isinstance(module, MultiheadSelfAttention):
+            if isinstance(module, UNet_AttentionBlock):
+                module.gradient_checkpointing = True
+                
+        for name, module in self.bottleneck.named_modules():
+            if isinstance(module, UNet_AttentionBlock):
+                module.gradient_checkpointing = True
+                
+        for name, module in self.decoder.named_modules():
+            if isinstance(module, UNet_AttentionBlock):
                 module.gradient_checkpointing = True
                 
     def forward(self, x: torch.Tensor, timestep: torch.LongTensor, cond: torch.Tensor) -> torch.Tensor:

train_lora_dreambooth.py
@@ -15,6 +15,8 @@ from transformers import CLIPTokenizer
 from utils.utils import load_model
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
+import gc
+
 
 def train_step(model: StableDiffusion,
                ema_model: EMA,
@@ -55,11 +57,11 @@ def train_step(model: StableDiffusion,
             # Actual noise
             timesteps = sampler._sample_timestep(imgs.shape[0]).to(device)
             x_t, actual_noise = sampler.forward_process(latent_features, timesteps)
-            actual_instance_noise, actual_class_prior_noise = actual_noise.chunk(2, dim=0)
+            
+        actual_instance_noise, actual_class_prior_noise = actual_noise.chunk(2, dim=0)
 
         # Predict noise
         pred_noise = model.unet(x_t, timesteps, text_embeddings)
-        
         pred_instance_noise, pred_class_prior_noise = pred_noise.chunk(2, dim=0)
         
         # Instance loss
@@ -73,9 +75,15 @@ def train_step(model: StableDiffusion,
         
         loss = loss / gradient_accumulation_steps
         loss.backward()
+        
         if ((i + 1) % gradient_accumulation_steps == 0) or (i + 1 == len(train_dataloader)):
             optimizer.step()
             optimizer.zero_grad()
+            if device == 'mps':
+                torch.mps.empty_cache()
+            elif device == 'cuda':
+                torch.cuda.empty_cache()
+            gc.collect()
         
         if ema_model is not None:
             ema_model.step(model)
@@ -220,18 +228,19 @@ if __name__ == '__main__':
                                                                     img_size=(args.img_size, args.img_size))
     
 
-    
-    train(model, 
-          tokenizer, 
-          train_dataloader, 
-          test_dataloader, 
-          epochs=300,
-          device=args.device, 
-          optimizer=optimizer, 
-          lr_scheduler=lr_scheduler, 
-          save_dir=args.save_dir, 
-          checkpoint_dir=args.checkpoint_dir, 
-          start_epoch=start_epoch,
-          use_lora=args.use_lora,
-          gradient_accumulation_steps=args.gradient_accumulation_steps,
-          gradient_checkpointing=args.gradient_checkpointing)
+    summary(model,
+            col_names=["num_params", "trainable"])
+    # train(model, 
+    #       tokenizer, 
+    #       train_dataloader, 
+    #       test_dataloader, 
+    #       epochs=300,
+    #       device=args.device, 
+    #       optimizer=optimizer, 
+    #       lr_scheduler=lr_scheduler, 
+    #       save_dir=args.save_dir, 
+    #       checkpoint_dir=args.checkpoint_dir, 
+    #       start_epoch=start_epoch,
+    #       use_lora=args.use_lora,
+    #       gradient_accumulation_steps=args.gradient_accumulation_steps,
+    #       gradient_checkpointing=args.gradient_checkpointing)